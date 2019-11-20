9eb391d36c647e13ff64e5ab89ff60eb0d7682ce
SasaJaic <jaic.sasa.bgd@gmail.com>
2019-11-20 13:07:40 +0100

    give some efects with jquery and AOS

--- main.js
+++ main.js
@@ -3,4 +3,25 @@ $().ready(function(){
       $(this).toggleClass('open');
       $('.top-nav').toggleClass('open');
    });
+   $('.top-nav .nav-link').on('click', function(){
+      $('.menu-toggler').removeClass('open');
+      $('.top-nav').removeClass('open');
+   });
+   
+   $('nav a[href*="#"]').on('click', function(){
+      $('html, body').animate(keyframes, {
+         scrollTop: $($(this).attr('href')).offset().top -100
+      }, options,2000);
+   });
+
+   $('#up').click(function() {
+      $("html, body").animate({ scrollTop: 0 }, 'slow');
+      return false;
+   });
+
+   AOS.init({
+      easing: 'ease',
+      duration: 1800,
+      once: true
+   });
 });
